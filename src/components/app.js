3d3f0e5d4d7823f7c2ccce528d068c43685a5e93
Slider7 <kemalsh771@gmail.com>
2017-01-23 17:59:58 +0600

    finish project

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,53 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import MDSpinner from 'react-md-spinner';
+
+import CamperList from './camperlist.js';
 
 export default class App extends Component {
-  render() {
-    return (
-      <div>React simple starter</div>
-    );
+  constructor(props) {
+    super(props);
+    this.state = {
+      recentCampers: [],
+      allTimeCampers: [],
+      currentView: 'recentCampers'
+    }
+  };
+
+  componentWillMount() {
+    axios.all([this.fetchRecentCampers(), this.fetchAllTimeCampers()])
+    .then(axios.spread((recentCampers, allTimeCampers) => {
+      /**/
+        this.setState({
+          recentCampers: recentCampers.data,
+          allTimeCampers: allTimeCampers.data
+        });
+      }));
   }
+
+  fetchRecentCampers() {
+    return axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/recent');
+  }
+
+  fetchAllTimeCampers() {
+    return axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/alltime');
+  }
+
+  changeView(currentView) {
+    this.setState({ currentView })
+  }
+
+  render() {
+    if (!this.state.recentCampers.length && !this.state.allTimeCampers.length) {
+      return <MDSpinner class ClassName="spinner" size={100} />;
+    }
+    return (
+      <div>
+        <h1>{`FCCLeaderBoard ${this.state.currentView}`}</h1>
+        <button onClick= { () => this.changeView('recentCampers')} className="btn btn-primary">Recent List</button>
+        <button onClick= { () => this.changeView('allTimeCampers')} className="btn btn-primary">All-time List</button>
+        <CamperList campers={this.state[this.state.currentView]} />
+      </div>
+    );
+  };
 }
